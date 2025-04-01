1fca781e55c821eb897a74df1681de9a2c775b25
Elamathi <130838524+ElamathiThirugnanam@users.noreply.github.com>
2025-04-01 15:52:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Sidebar from './Component/Dashboard/Sidebar/Sidebar';
+import './App.css';
+import DashboardPage from './Component/Dashboard/DashboardPage';
+import Adminpage from './Component/Dashboard/Adminpage';
+import CardArray from './Component/Dashboard/CardArray';
+
+function App() {
+  return (
+    <>
+   <DashboardPage/>
+    </>
+  )
+}
+
+export default App;
